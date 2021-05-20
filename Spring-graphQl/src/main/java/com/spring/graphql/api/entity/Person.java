4a2e085633975d231f5650305d70589b8ee622ce
Bhushan Patil <bhupatil0001@gmail.com>
2021-05-20 12:56:16 +0530

    Added: H2 database

--- Spring-graphQl/src/main/java/com/spring/graphql/api/entity/Person.java
+++ Spring-graphQl/src/main/java/com/spring/graphql/api/entity/Person.java
@@ -3,12 +3,19 @@ package com.spring.graphql.api.entity;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
-
+import lombok.ToString;
+@Data
 @Getter
 @Setter
 @Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
 public class Person {
 	@Id
 	private int id;
